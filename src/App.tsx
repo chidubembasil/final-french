170c084ea487b1f6724ffec246cf5a6105c4a379
BASIL CHIDUBEM <chidbasil@gmail.com>
2026-01-03 15:57:08 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
-
 import "./index.css";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 import CookieConsent from "./components/CookieConsent";
 
-// ✅ Lazy-loaded pages
-const Home = lazy(() => import("./pages/Home"));
-const Podcast = lazy(() => import("./pages/Podcast"));
-const Resources = lazy(() => import("./pages/Resources"));
-const Gallery = lazy(() => import("./pages/Gallery"));
-const News = lazy(() => import("./pages/News"));
-const BAC = lazy(() => import("./pages/Bac"));
-const Activites = lazy(() => import("./pages/Activity"));
+// ✅ Standard imports (No more lazy loading)
+import Home from "./pages/Home";
+import Podcast from "./pages/Podcast";
+import Resources from "./pages/Resources";
+import Gallery from "./pages/Gallery";
+import News from "./pages/News";
+import BAC from "./pages/Bac";
+import Activites from "./pages/Activity";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Header />
 
-      {/* ✅ Suspense wraps ONLY the routes */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/podcast" element={<Podcast />} />
-          <Route path="/resource" element={<Resources />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/news&blog" element={<News />} />
-          <Route path="/bac" element={<BAC />} />
-          <Route path="/activities" element={<Activites />} />
-        </Routes>
-      </Suspense>
+      {/* Suspense is no longer needed for routes since we aren't lazy loading */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/podcast" element={<Podcast />} />
+        <Route path="/resource" element={<Resources />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/news&blog" element={<News />} />
+        <Route path="/bac" element={<BAC />} />
+        <Route path="/activities" element={<Activites />} />
+      </Routes>
 
       <Footer />
       <CookieConsent />
